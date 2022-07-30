c6b3ef79d3fdab466d2ab2e86916988552de65e0
kishan1988-hub <69946669+kishan1988-hub@users.noreply.github.com>
2022-07-30 23:29:34 +0530

    missed to perform fmt

--- meta-arg.tf
+++ meta-arg.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "pub_sub" {
   count = length(var.pub_sub)
 
-  vpc_id      = aws_vpc.myvpc.id
+  vpc_id     = aws_vpc.myvpc.id
   cidr_block = var.pub_sub[count.index]
 
   tags = {
