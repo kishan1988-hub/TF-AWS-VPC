c807e034ae5f1f0cfdf205085b35d66ed81df15e
kishan1988-hub <69946669+kishan1988-hub@users.noreply.github.com>
2022-08-04 23:08:47 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,89 @@
+data "aws_ami" "example" {
+  owners      = ["137112412989"]
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-2018.03.0.20180811-x86*"]
+  }
+}
+
+resource "aws_instance" "public" {
+  ami                         = data.aws_ami.example.id
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  key_name                    = "main"
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  subnet_id                   = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}-public"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from Public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["106.195.42.36/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                    = "ami-0912f71e06545ad88"
+  instance_type          = "t2.micro"
+  key_name               = "dropmailtokishan"
+  vpc_security_group_ids = [aws_security_group.private.id]
+  subnet_id              = aws_subnet.private[0].id
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
+
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
