7d2cd36793db83657fb2b386a2d564c1c7cbb23a
kishan1988-hub <69946669+kishan1988-hub@users.noreply.github.com>
2022-08-05 18:23:53 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,20 +1,20 @@
-data "aws_ami" "example"{
-  owners = ["137112412989"]
-  most_recent      = true
+data "aws_ami" "amazonlinux" {
+  owners      = ["137112412989"]
+  most_recent = true
 
   filter {
-  name   = "virtualization-type"
-  values = ["hvm"]
-}
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-filter {
-  name   = "name"
-  values = ["amzn-ami-hvm-2018.03.0.20180811-x86*"]
-}
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-2018.03.0.20180811-x86*"]
+  }
 }
 
 resource "aws_instance" "public" {
-  ami                         =  data.aws_ami.example.id
+  ami                         = data.aws_ami.amazonlinux.id
   associate_public_ip_address = true
   instance_type               = "t2.micro"
   key_name                    = "main"
