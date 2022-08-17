9c906759e09ace4362dae730c08457ae9f11c7e1
Vladimir <awsvlad@gmail.com>
2022-08-16 22:45:25 -0400

    Add load balancer

--- level2/load_balancer.tf
+++ level2/load_balancer.tf
@@ -55,19 +55,19 @@ resource "aws_lb_target_group" "main" {
 
 
 resource "aws_lb_target_group_attachment" "main" {
-  target_group_arn = [aws_lb_target_group.main.arn]
-  target_id        = [aws_instance.private.id]
+  target_group_arn = aws_lb_target_group.main.arn
+  target_id        = aws_instance.private.id
   port             = 80
 }
 
 
 resource "aws_lb_listener" "main" {
-  load_balancer_arn = [aws_lb.main.arn]
+  load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = [aws_lb_target_group.main.arn]
+    target_group_arn = aws_lb_target_group.main.arn
   }
 }
