c697699b458aea6cd8913e0fd463da0f31f3600b
kishan1988-hub <69946669+kishan1988-hub@users.noreply.github.com>
2022-08-23 17:18:23 +0530

    formatting

--- level2/iam.tf
+++ level2/iam.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "main" {
-    name = "${var.env_code}-s3-role"
-    assume_role_policy = "${file("assumerole.json")}"
+  name               = "${var.env_code}-s3-role"
+  assume_role_policy = file("assumerole.json")
 }
 
 
 resource "aws_iam_policy" "main" {
-    name = "${var.env_code}-s3-policy"
-    description = "S3 bucket full access"
-    policy = "${file("s3bucket.json")}"
+  name        = "${var.env_code}-s3-policy"
+  description = "S3 bucket full access"
+  policy      = file("s3bucket.json")
 }
 
 resource "aws_iam_role_policy_attachment" "main" {
-    role = aws_iam_role.main.name
-    policy_arn = aws_iam_policy.main.arn
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.main.arn
 }
 
 
 resource "aws_iam_instance_profile" "main" {
-    name = "${var.env_code}"
-    role = aws_iam_role.main.name
+  name = var.env_code
+  role = aws_iam_role.main.name
 }
