848c48058a9ce722489838c48490264e15099b1d
kishan1988-hub <69946669+kishan1988-hub@users.noreply.github.com>
2022-08-25 12:14:44 +0530

    Update iam.tf

--- level2/iam.tf
+++ level2/iam.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role" "main" {
   assume_role_policy = file("assumerole.json")
 }
 
-
+/*
 resource "aws_iam_policy" "main" {
   name        = "${var.env_code}-s3-policy"
   description = "S3 bucket full access"
@@ -14,7 +14,7 @@ resource "aws_iam_role_policy_attachment" "main" {
   role       = aws_iam_role.main.name
   policy_arn = aws_iam_policy.main.arn
 }
-
+*/
 resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.main.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
