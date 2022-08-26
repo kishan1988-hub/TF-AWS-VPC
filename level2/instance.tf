825bd8f91aec16c8f8c55856de9858b2e9b8007d
kishan1988-hub <69946669+kishan1988-hub@users.noreply.github.com>
2022-08-27 00:28:16 +0530

    modules

--- level2/instance.tf
+++ level2/instance.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "public" {
   ami                         = data.aws_ami.amazonlinux.id
   associate_public_ip_address = true
   instance_type               = "t2.micro"
-  key_name                    = "main"
+  key_name                    = "dropmailtokishan"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[1]
   user_data                   = file("userdata.sh")
@@ -46,40 +46,6 @@ resource "aws_security_group" "public" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["106.208.18.112/32"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-}
-
-
-
-resource "aws_security_group" "private" {
-  name        = "${var.env_code}-private"
-  description = "Allow inbound traffic"
-  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr_block]
-  }
-  ingress {
-    description     = "HTTP from load_balancer"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.load_balancer.id]
   }
 
   egress {
